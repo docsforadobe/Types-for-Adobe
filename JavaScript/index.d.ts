a45c676b6c128ce0bca4ae9b9c08e8496b5d2b24
pravdomil <pravdomil.toman@gmail.com>
2017-08-27 12:56:48 +0200

    minor

--- JavaScript/index.d.ts
+++ JavaScript/index.d.ts
@@ -280,7 +280,7 @@ interface ArrayConstructor {
 	/**
 	 * Creates and returns a new array.
 	 * Takes any number of parameters, which become the elements of the array, or a single value which becomes the length of an empty array. Note that you cannot create a one-element array, as the single parameter value is interpreted as the length. Returns the new array.
-	 * @param length If no other parameters are passed, the initial length of the empty array. Otherwise, the first element.
+	 * @param arrayLength If no other parameters are passed, the initial length of the empty array. Otherwise, the first element.
 	 * @param values If there is more than one parameter, the array is initialized with the given parameters.
 	 */
 	new(arrayLength?: number): any[];
