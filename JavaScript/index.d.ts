adc8d9293a31eb6f4c0496d788a3fb7edaa4e368
pravdomil <pravdomil.toman@gmail.com>
2017-09-05 16:10:29 +0200

    Partial, Readonly, Pick, Record

--- JavaScript/index.d.ts
+++ JavaScript/index.d.ts
@@ -2693,3 +2693,31 @@ interface IArguments {
 	length: number;
 	callee: Function;
 }
+
+/**
+ * Make all properties in T optional
+ */
+type Partial<T> = {
+    [P in keyof T]?: T[P];
+};
+
+/**
+ * Make all properties in T readonly
+ */
+type Readonly<T> = {
+    readonly [P in keyof T]: T[P];
+};
+
+/**
+ * From T pick a set of properties K
+ */
+type Pick<T, K extends keyof T> = {
+    [P in K]: T[P];
+};
+
+/**
+ * Construct a type with a set of properties K of type T
+ */
+type Record<K extends string, T> = {
+    [P in K]: T;
+};
