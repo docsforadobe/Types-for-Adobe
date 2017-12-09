0f233d43dbaee1edd2851efbd6d44591876ae7a6
pravdomil <pravdomil.toman@gmail.com>
2017-12-09 21:27:52 +0100

    fix indent

--- JavaScript/index.d.ts
+++ JavaScript/index.d.ts
@@ -2700,26 +2700,26 @@ interface IArguments {
  * Make all properties in T optional
  */
 type Partial<T> = {
-    [P in keyof T]?: T[P];
+	[P in keyof T]?: T[P];
 };
 
 /**
  * Make all properties in T readonly
  */
 type Readonly<T> = {
-    readonly [P in keyof T]: T[P];
+	readonly [P in keyof T]: T[P];
 };
 
 /**
  * From T pick a set of properties K
  */
 type Pick<T, K extends keyof T> = {
-    [P in K]: T[P];
+	[P in K]: T[P];
 };
 
 /**
  * Construct a type with a set of properties K of type T
  */
 type Record<K extends string, T> = {
-    [P in K]: T;
+	[P in K]: T;
 };
