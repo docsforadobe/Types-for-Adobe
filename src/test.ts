67aff444cb151c432244937d54ac415392aa1dd7
pravdomil <pravdomil.toman@gmail.com>
2017-12-10 00:23:40 +0100

    test

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,30 @@
+import * as child_process from "child_process";
+import { IOptions, sync as glob } from "glob";
+import { promisify } from "util";
+const exec = promisify(child_process.exec);
 
 main();
 
-function main() {
-    return 1;
+async function main() {
+    try {
+        await test();
+        console.log("Test passed!");
+        
+    } catch (e) {
+        console.error(e.cmd);
+        console.error(e.stdout);
+        process.exit(1);
+    }
+}
+
+async function test() {
+    const options: IOptions = { ignore: "node_modules/**" };
+    const files = glob("*/**/tsconfig.json", options);
+
+    const queue = files.map((file) => {
+        const cwd = file.replace("tsconfig.json", "");
+        return exec("cd " + cwd + "; tsc -p .");
+    });
+    
+    await Promise.all(queue);
 }
