c4c2a15785421f8eb1213b90bc313b82d73e5115
pravdomil <pravdomil.toman@gmail.com>
2017-12-09 21:15:24 +0100

    fix #17

--- Photoshop/2015.5/index.d.ts
+++ Photoshop/2015.5/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="../../global/index.d.ts" />
+/// <reference path="../../ScriptUI/index.d.ts" />
 
 /**
  * Document formats that Photoshop can open.
@@ -10963,37 +10964,6 @@ declare class XMPMetadata {
 	 * The class name of the object.
 	 */
 	readonly typename: string;
-
-}
-
-/**
- * Describes a point. This class is also a two-element collection.
- */
-declare class Point {
-	/**
-	 * The left coordinate.
-	 */
-	left: number;
-
-	/**
-	 * The array length.
-	 */
-	readonly length: number;
-
-	/**
-	 * The top coordinate.
-	 */
-	top: number;
-
-	/**
-	 * The left coordinate.
-	 */
-	x: number;
-
-	/**
-	 * The top coordinate.
-	 */
-	y: number;
 
 }
 
