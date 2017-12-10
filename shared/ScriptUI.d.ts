c50c520b744ffd28c506683afc3110a4e4b5449f
pravdomil <pravdomil.toman@gmail.com>
2017-12-10 23:09:15 +0100

    fix

--- shared/ScriptUI.d.ts
+++ shared/ScriptUI.d.ts
@@ -256,7 +256,7 @@ declare class Window extends _Control {
 	/**
 	 * Closes this window.
 	 * . If an onClose() callback is defined for the window, calls that function before closing the window.
-	 * @param return A number to be returned from the show() method that invoked this window as a modal dialog.
+	 * @param return_ A number to be returned from the show() method that invoked this window as a modal dialog.
 	 */
 	close(return_?: any): void;
 
