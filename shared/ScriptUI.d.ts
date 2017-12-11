f2883164aac2bc53967430bcdfe96cf91f8488fc
pravdomil <pravdomil.toman@gmail.com>
2017-12-11 13:03:23 +0100

    DropDownList selection can be number

--- shared/ScriptUI.d.ts
+++ shared/ScriptUI.d.ts
@@ -1250,7 +1250,7 @@ declare class DropDownList extends _Control {
 	 * The currently selectedlist item.
 	 * Setting this value causes the selected item to be highlighted and to be scrolled into view if necessary. If no items are selected, the value is null. Set to null to deselect all items.You can set the value using the index of an item, rather than an object reference. If set to an index value that is out of range, the operation is ignored. When set with an index value, the property still returns an object reference.
 	 */
-	selection: ListItem;
+	selection: ListItem | number;
 
 	/**
 	 * The key sequence that invokes the onShortcutKey() callback for this element (in Windows only).
