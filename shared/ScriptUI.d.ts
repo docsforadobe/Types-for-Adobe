9a28b81217f4949af65b6f250df69fdc9f0db99a
Pravdomil <pravdomil.toman@gmail.com>
2017-12-15 14:23:59 +0100

    opt

--- shared/ScriptUI.d.ts
+++ shared/ScriptUI.d.ts
@@ -2191,14 +2191,12 @@ declare class ObjectPoint {
 
 }
 
-declare type ArrayPoint = [number, number]
-
 /**
  * Defines the location of a window or UI element. Contains a 2-element array.
  * Specifies the origin point of an element as horizontal and vertical pixel offsets from the origin of the element's coordinate space.
  * A Point object is created when you set an element’s location property. You can set the property using a JavaScript object with properties named x and y, or an array with 2 values in the order [x, y].
  */
-declare type Point = ArrayPoint | ObjectPoint
+declare type Point = ObjectPoint | [number, number]
 
 declare class ObjectDimension {
 	/**
