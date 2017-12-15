35c76cd649637a2c061cc8aa152f00cd689bb31b
Pravdomil <pravdomil.toman@gmail.com>
2017-12-15 14:26:51 +0100

    opt

--- shared/ScriptUI.d.ts
+++ shared/ScriptUI.d.ts
@@ -2162,7 +2162,12 @@ declare class Panel extends _Control {
 
 }
 
-
+/**
+ * Defines the location of a window or UI element. Contains a 2-element array.
+ * Specifies the origin point of an element as horizontal and vertical pixel offsets from the origin of the element's coordinate space.
+ * A Point object is created when you set an element’s location property. You can set the property using a JavaScript object with properties named x and y, or an array with 2 values in the order [x, y].
+ */
+declare type Point = ObjectPoint | [number, number]
 declare class ObjectPoint {
 	/**
 	 * The left coordinate.
@@ -2190,13 +2195,6 @@ declare class ObjectPoint {
 	y: number;
 
 }
-
-/**
- * Defines the location of a window or UI element. Contains a 2-element array.
- * Specifies the origin point of an element as horizontal and vertical pixel offsets from the origin of the element's coordinate space.
- * A Point object is created when you set an element’s location property. You can set the property using a JavaScript object with properties named x and y, or an array with 2 values in the order [x, y].
- */
-declare type Point = ObjectPoint | [number, number]
 
 declare class ObjectDimension {
 	/**
