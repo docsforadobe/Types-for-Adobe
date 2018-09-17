56279029ed444ff679c2ba50d31efa9c802deebe
Koen Schmeets <k.schmeets@gmail.com>
2018-09-17 15:46:50 +0200

    Seperate PlugPlugExternalObject types

---
+++ shared/PlugPlugExternalObject.d.ts
@@ -0,0 +1,38 @@
+interface CSXSEvent {
+	/**
+	 * Retrieves the unique identifier of the application from which this event was dispatched.
+	 */
+	readonly appId: string;
+
+	/**
+	 * Retrieves or sets the payload of this event.
+	 */
+	data: string;
+
+	/**
+	 * Retrieves the unique identifier of the extension from which this event was dispatched.
+	 */
+	readonly extensionId: string;
+
+	/**
+	 * Retrieves the scope of this event.
+	 */
+	scope: string;
+
+	/**
+	 * Dispatch the event
+	 */
+	dispatch(): void;
+}
+
+interface CSXSEventConstructor {
+	readonly prototype: CSXSEvent;
+
+	/**
+	 * Creates a new CSXSEvent object.
+	 */
+	new(type?: string, scope?: string, data?: string): CSXSEvent;
+	(type?: string, scope?: string, data?: string): CSXSEvent;
+}
+
+declare const CSXSEvent: CSXSEventConstructor;
