f239e35e6be8800498d31b7130a14f03ca2c4045
Nik Ska <dokluch@gmail.com>
2019-02-20 16:00:24 +0300

    Added Team Projects syntax for After Effects

--- AfterEffects/2018/index.d.ts
+++ AfterEffects/2018/index.d.ts
@@ -19,6 +19,8 @@ declare const enum AppVersion {
   CC2015_2 = 13.7,
   CC2015_3 = 13.8,
   CC2017 = 14.0,
+  CC2018 = 15.0,
+  CC2019 = 16.0
 }
 
 declare const enum CommandID {
@@ -546,6 +548,12 @@ declare enum PropertyType {
   PROPERTY,
   INDEXED_GROUP,
   NAMED_GROUP,
+}
+
+declare enum ResolveType {
+  ACCEPT_THEIRS,
+  ACCEPT_YOURS,
+  ACCEPT_THEIRS_AND_COPY,
 }
 
 declare enum RQItemStatus {
@@ -1730,6 +1738,65 @@ declare class Project {
 
   /** Automatically replaces text in all expressions. */
   autoFixExpressions(oldText: string, newText: string): void
+
+  /** Creates a new team project. */
+  newTeamProject(
+    teamProjectName: string, 
+    description: string
+  ): boolean
+  
+  /** Opens a team project. */
+  openTeamProject(
+    teamProjectName: string
+  ): boolean
+
+  /** Shares the currently open team project. */
+  shareTeamProject(
+    comment: string
+  ): boolean
+
+  /** Syncs the currently open team project. */
+  syncTeamProject(): boolean
+
+  /** Closes a currently open team project. */
+  closeTeamProject(): boolean
+  
+  /** Converts a team project to an After Effects project on a local disk. */
+  convertTeamProjectToProject(
+    project_file: File
+  ): boolean
+
+  /** Returns an array containing the name strings for all team projects available for the current user. Archived Team Projects are not included. */
+  listTeamProjects(): string[]
+
+  /** Checks whether specified team project is currently open. */
+  isTeamProjectOpen(
+    teamProjectName: string
+  ): boolean
+
+  /** Checks whether any team project is currently open. */
+  isAnyTeamProjectOpen(): boolean
+
+  /** Checks whether or not team projects is enabled for After Effects. (This will almost always return true.) */
+  isTeamProjectEnabled(): boolean
+
+  /** Checks whether or not the client (After Effects) is currently logged into the team project server. */
+  isLoggedInToTeamProject(): boolean
+
+  /** Checks whether or not the Sync command is enabled. */
+  isSyncCommandEnabled(): boolean
+
+  /** Checks whether or not the Share command is enabled. */
+  isShareCommandEnabled(): boolean
+
+  /** Checks whether or not the Resolve command is enabled. */
+  isResolveCommandEnabled(): boolean
+
+  /** Resolves a conflict between the open team project and the version on the team projects server, using the specified resolution method. */
+  resolveConflict(
+    ResolveType: ResolveType
+  ): boolean
+
 }
 
 declare type PropertyValue =
