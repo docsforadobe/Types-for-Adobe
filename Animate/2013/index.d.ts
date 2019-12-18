29542babd42e6462650ac2edd8929826ff7955f7
Casey Stinnett <casey.stinnett@pearson.com>
2019-12-18 15:03:51 -0700

    Capitalized Flash class

--- Animate/2013/index.d.ts
+++ Animate/2013/index.d.ts
@@ -635,7 +635,7 @@ declare class FLfile {
 /**
  * The flash object represents the Flash application. You can use flash or fl to refer to this object. This documentation uses fl in code samples throughout.
  */
-interface flash {
+interface Flash {
   /**
    * An actionsPanel object, which represents the currently displayed Actions panel. For information on using this property, see actionsPanel object.
    */
@@ -1108,12 +1108,12 @@ interface flash {
    */
   xmlPanelFromString(xmlString: string): XMLUI;
 }
-declare const flash: flash;
+declare const flash: Flash;
 /**
  * Short variable name for flash.
  */
-interface fl extends flash { }
-declare const fl: flash;
+interface fl extends Flash { }
+declare const fl: Flash;
 /**
  * The SpriteSheetExporter object is a subclass of the
 Item object.
