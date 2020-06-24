abf821a168dadead98798f676f31b3d059a0c3cf
pravdomil <pravdomil.toman@gmail.com>
2020-06-24 12:13:11 +0200

    add Tab and TabbedPanel

--- shared/ScriptUI.d.ts
+++ shared/ScriptUI.d.ts
@@ -2111,6 +2111,43 @@ declare class Panel extends _Control {
 }
 
 /**
+ * A container for other types of controls.
+ * Differs from a panel element in that is must be a direct child of a tabbedpanel element, the title is shown in the selection tab, and it does not have a script-definable border.
+ */
+declare class Tab extends Panel {
+  /**
+   * The parent element.
+   */
+  readonly parent: TabbedPanel
+}
+
+/**
+ * A container for selectable tab containers.
+ * Differs from a panel element in that it can contain only tab elements as direct children.
+ */
+declare class TabbedPanel extends Panel {
+  /**
+   * An array of child elements.
+   */
+  readonly children: Tab[]
+
+  /**
+   * The currently selected tab.
+   * Setting this value causes the specified tab to be enabled in the panel.
+   * You can set the value using the index of an item, rather than an object reference.
+   * If set to an index value that is out of range, the operation is ignored.
+   * When set with an index value, the property still returns an object reference.
+   * When the value of the selection property changes, either by a user selecting a different tab, or by a script setting the property, the TabbedPanel receives an onChange notification.
+   */
+  selection: Tab | number
+
+  /**
+   * An event-handler callback function, called when the selected tab has changed.
+   */
+  onChange(): void
+}
+
+/**
  * Defines the location of a window or UI element. Contains a 2-element array.
  * Specifies the origin point of an element as horizontal and vertical pixel offsets from the origin of the element's coordinate space.
  * A Point object is created when you set an element’s location property. You can set the property using a JavaScript object with properties named x and y, or an array with 2 values in the order [x, y].
@@ -2861,6 +2898,20 @@ interface _AddControl {
     text?: string,
     properties?: Partial<_AddControlPropertiesStaticText>,
   ): StaticText
+
+  (
+    type: "tab",
+    bounds: Bounds | [number, number, number, number],
+    text?: string[],
+    properties?: Partial<_AddControlProperties>,
+  ): Tab
+
+  (
+    type: "tabbedpanel",
+    bounds?: Bounds | [number, number, number, number],
+    text?: string,
+    properties?: Partial<_AddControlProperties>,
+  ): TabbedPanel
 
   (
     type: "treeview",
