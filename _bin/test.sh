5f565b7dde3b010d96a20105ae4cc688dfa6a8f5
Justin Taylor <justin2taylor@gmail.com>
2022-01-27 10:08:58 -0800

    updated test.sh

--- _bin/test.sh
+++ _bin/test.sh
@@ -13,8 +13,26 @@ set -x
 cd "${0%/*}/.."
 
 # To test if everything compiles.
+tsc --pretty --noEmit -p ./AfterEffects/8.0
+tsc --pretty --noEmit -p ./AfterEffects/9.0
+tsc --pretty --noEmit -p ./AfterEffects/10.5
+tsc --pretty --noEmit -p ./AfterEffects/11.0
+tsc --pretty --noEmit -p ./AfterEffects/12.0
+tsc --pretty --noEmit -p ./AfterEffects/13.0
+tsc --pretty --noEmit -p ./AfterEffects/13.1
+tsc --pretty --noEmit -p ./AfterEffects/13.2
+tsc --pretty --noEmit -p ./AfterEffects/13.6
+tsc --pretty --noEmit -p ./AfterEffects/13.8
+tsc --pretty --noEmit -p ./AfterEffects/14.0
+tsc --pretty --noEmit -p ./AfterEffects/14.2
+tsc --pretty --noEmit -p ./AfterEffects/15.0
+tsc --pretty --noEmit -p ./AfterEffects/16.0
+tsc --pretty --noEmit -p ./AfterEffects/16.1
+tsc --pretty --noEmit -p ./AfterEffects/17.0
 tsc --pretty --noEmit -p ./AfterEffects/17.1
-tsc --pretty --noEmit -p ./Animate/2013
+tsc --pretty --noEmit -p ./AfterEffects/18.0
+tsc --pretty --noEmit -p ./Animate/13.0
+tsc --pretty --noEmit -p ./Animate/22.0
 tsc --pretty --noEmit -p ./Audition/2015.2
 tsc --pretty --noEmit -p ./Audition/2017
 tsc --pretty --noEmit -p ./Audition/2018
@@ -22,4 +40,8 @@ tsc --pretty --noEmit -p ./Illustrator/2015.3
 tsc --pretty --noEmit -p ./InDesign/2015.3
 tsc --pretty --noEmit -p ./InDesign/2018
 tsc --pretty --noEmit -p ./Photoshop/2015.5
-tsc --pretty --noEmit -p ./Premiere/2018
+tsc --pretty --noEmit -p ./Premiere/11.0
+tsc --pretty --noEmit -p ./Premiere/12.0
+tsc --pretty --noEmit -p ./Premiere/13.0
+tsc --pretty --noEmit -p ./Premiere/14.0
+tsc --pretty --noEmit -p ./Premiere/15.0
