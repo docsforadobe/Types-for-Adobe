ca74902b591fae5d7881026ef130fc143dde9e37
Martin Lindberg <30564420+fartinmartin@users.noreply.github.com>
2022-08-31 08:38:00 -0700

    attempt to add typedoc

---
+++ typedoc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entryPoints: [
+    "./AfterEffects/18.0/index.d.ts",
+    "./Animate/22.0/index.d.ts",
+    "./Audition/2018/index.d.ts",
+    "./Illustrator/2015.3/index.d.ts",
+    "./InDesign/2018/index.d.ts",
+    "./Photoshop/2015.5/index.d.ts",
+    "./Premiere/15.0/index.d.ts",
+  ],
+  // entryPointStrategy: "expand",
+}
