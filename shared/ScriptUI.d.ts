c815678e7af3076c46a6e3028601dce50b2aa93e
Martin Lindberg <30564420+fartinmartin@users.noreply.github.com>
2025-05-10 10:29:59 -0700

    `onClose` _can_ return false to cancel, but it is not required

--- shared/ScriptUI.d.ts
+++ shared/ScriptUI.d.ts
@@ -346,7 +346,7 @@ declare class Window extends _Control {
    * An event-handler callback function, called when the window is about to be closed.
    * Called when a request is made to close the window, either by an explicit call to the close() function or by a user action (clicking the OS-specific close icon in the title bar). The function is called before the window actually closes; it can return false to cancel the close operation.
    */
-  onClose(): boolean
+  onClose(): void | boolean
 
   /**
    * An event-handler callback function, called when the window loses the keyboard focus.
